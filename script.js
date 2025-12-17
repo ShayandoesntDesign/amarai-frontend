820c3aee377eafb734e257cbd8a680af68ad4f8e
Shayan <shayan2001official@gmail.com>
2025-12-17 17:35:00 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const chatBox = document.getElementById('chat-box');
+const input = document.getElementById('user-input');
+const button = document.getElementById('send-btn');
+
+button.addEventListener('click', async () => {
+  const question = input.value;
+  if (!question) return;
+  
+  chatBox.innerHTML += `<p><b>You:</b> ${question}</p>`;
+  input.value = '';
+
+  try {
+    const res = await fetch('https://amarai-backend.yourrepl.co/api/ask', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ question })
+    });
+    const data = await res.json();
+    chatBox.innerHTML += `<p><b>AmarAI:</b> ${data.answer}</p>`;
+  } catch (err) {
+    chatBox.innerHTML += `<p><b>AmarAI:</b> Error connecting to backend</p>`;
+  }
+});
