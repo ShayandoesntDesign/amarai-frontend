f88ed11f2e50bce6d831f7a1725daa58813c5eff
Shayan <shayan2001official@gmail.com>
2025-12-17 17:49:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const button = document.getElementById('send-btn');
 button.addEventListener('click', async () => {
   const question = input.value;
   if (!question) return;
-  
+
   chatBox.innerHTML += `<p><b>You:</b> ${question}</p>`;
   input.value = '';
 
@@ -15,9 +15,13 @@ button.addEventListener('click', async () => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ question })
     });
+
     const data = await res.json();
     chatBox.innerHTML += `<p><b>AmarAI:</b> ${data.answer}</p>`;
+    chatBox.scrollTop = chatBox.scrollHeight;
+
   } catch (err) {
     chatBox.innerHTML += `<p><b>AmarAI:</b> Error connecting to backend</p>`;
   }
 });
+
